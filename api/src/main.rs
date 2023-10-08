5a2dbc32bda310a4b6707411242b90b011af6847
Qin Guan <qinguan20040914@gmail.com>
2023-10-08 13:19:38 +0800

    feat: basic axum

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use log::info;
+use std::net::SocketAddr;
+
+use dotenvy::{dotenv, var};
+
+mod routes;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    pretty_env_logger::init();
+
+    let app = Router::new().route("/", get(routes::index::handler));
+
+    let port = var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let addr = SocketAddr::from(([127, 0, 0, 1], port.parse().unwrap()));
+
+    info!("API listening on {}", port);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
